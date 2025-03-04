89b27edf031f33e30f088102d30bfa1eac46098c
Régis Daum <rdaum@john-steel.com>
2025-03-04 20:52:03 +0100

    Initial commit with auth

---
+++ auth.js
@@ -0,0 +1,71 @@
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { db } from "./db";
+import { saltAndHashPassword } from "./utils/helper";
+
+export const {
+	handlers: { GET, POST },
+	signIn,
+	signOut,
+	auth,
+} = NextAuth({
+	adapter: PrismaAdapter(db),
+	session: { strategy: "jwt" },
+	providers: [
+		Github({
+			clientId: process.env.AUTH_GITHUB_ID,
+			clientSecret: process.env.AUTH_GITHUB_SECRET,
+		}),
+		Credentials({
+			name: "Credentials",
+			credentials: {
+				email: {
+					label: "Email",
+					type: "email",
+					placeholder: "email@example.com",
+				},
+				password: { label: "Password", type: "password" },
+			},
+			authorize: async (credentials) => {
+				if (
+					!credentials ||
+					!credentials.email ||
+					!credentials.password
+				) {
+					return null;
+				}
+
+				const email = credentials.email;
+				const hash = saltAndHashPassword(credentials.password);
+
+				let user = await db.user.findUnique({
+					where: {
+						email,
+					},
+				});
+
+				if (!user) {
+					user = await db.user.create({
+						data: {
+							email,
+							hashedPassword: hash,
+						},
+					});
+				} else {
+					const isMatch = bcrypt.compareSync(
+						credentials.password,
+						user.hashedPassword
+					);
+					if (!isMatch) {
+						throw new Error("Incorrect password.");
+					}
+				}
+
+				return user;
+			},
+		}),
+	],
+});
